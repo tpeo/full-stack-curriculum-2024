7d1c9ed262402f5c71b9650931e271b459aa83f4
ShivanshNikhra <shiv.nikhra@gmail.com>
2024-06-19 13:33:15 -0500

    Added Starter Template for Demo

---
+++ Week5/PokemonDemo/pokemon-demo-template/src/Homepage.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Homepage = () => {
+    const pokemonList = [
+        "bulbasaur",
+        "ivysaur",
+        "venusaur",
+        "charmander",
+        "charmeleon",
+        "charizard",
+        "squirtle",
+        "wartortle",
+        "blastoise",
+        "caterpie",
+        "metapod",
+        "butterfree",
+        "weedle",
+        "kakuna",
+    ];
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Homepage
